1181da49fb46ab9f9ca6bc76637f9144f5ca739f
Trent Petersen <trentpetersen@gmail.com>
2015-02-22 19:37:48 -0800

    added 2nd round of content. footer added

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,14 @@ $(document).foundation();
 
 $(document).ready(function(){
 
-
+	new Maplace({
+		show_markers: true,
+		locations: [{
+			lat: 47.7133029, 
+			lon: -117.4071151,
+			zoom: 16
+		}]
+	}).Load();
 
 	
 
